77c5022faf734f7fc606a066b9a84e456d6fee57
Long Nguyen <olragon@gmail.com>
2015-04-02 16:31:54 +0700

    Upgrade for Meteor 0.9+

--- package.js
+++ package.js
@@ -1,8 +1,16 @@
 Package.describe({
-  summary: "URI.js"
+  name: 'olragon:uri-js',
+  summary: "URI.js repackaged for Meteor",
+  version: '1.13.1',
+  git: 'https://github.com/olragon/meteor-uri-js.git',
+  documentation: 'README.md',
 });
 
 Package.on_use(function (api, where) {
+  if (api.versionsFrom) {
+    api.versionsFrom('0.9.0');
+  }
+  
   where = where || ['client', 'server'];
 
   api.add_files('lib/uri.js/src/URI.js', 'server');
